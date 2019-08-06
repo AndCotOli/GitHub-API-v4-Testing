e6869391aae5cc584cb5f24d12e66e635d61bea8
Andreu Cotanda Oliver <cotoli23@gmail.com>
2019-08-06 23:58:17 +0200

    Do basic query

---
+++ index.js
@@ -0,0 +1,29 @@
+const { GraphQLClient } = require("graphql-request");
+const { token } = require("./config.json");
+
+async function main() {
+  const url = "https://api.github.com/graphql";
+  const options = {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  };
+
+  const client = new GraphQLClient(url, options);
+
+  const query = `
+ {
+   viewer {
+     login
+     organization(login: "Coding Garden Community") {
+       id
+     }
+   }
+ }
+`;
+
+  const data = await client.request(query);
+  console.log(JSON.stringify(data, undefined, 2));
+}
+
+main();
